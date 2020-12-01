518778de86749f9118b131c4a3a85311f7e4756a
Jan Balangue <jan.a.balangue@gmail.com>
2020-11-30 19:29:10 -0600

    Empty project

---
+++ brewery/src/main/java/io/github/computeruser88/brewery/BreweryApplication.java
@@ -0,0 +1,13 @@
+package io.github.computeruser88.brewery;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BreweryApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BreweryApplication.class, args);
+	}
+
+}
